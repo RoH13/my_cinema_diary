6ca141e69ee38964b2c6cbf7cb4cf1b6afc01ffc
raman <romguzen@gmail.com>
2025-10-26 02:09:14 +0300

    modified:   src/main/java/com/cinema/dao/MovieDAO.java 	modified:   src/main/java/com/cinema/model/Movie.java

--- src/main/java/com/cinema/dao/MovieDAO.java
+++ src/main/java/com/cinema/dao/MovieDAO.java
@@ -1,14 +1,27 @@
 package com.cinema.dao;
 
+import com.cinema.model.Movie;
 import com.cinema.util.DataBaseConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MovieDAO {
     Connection connection = DataBaseConnection.getConnection();
-
+    String nextId = "select coalesce(max(id), 0) from movie;";
+    String indertMpvie = "insert into "
     public MovieDAO() throws SQLException {
 
     }
+    public int insertMovie(String title, String name, String genre, int year, int duration, int raiting) throws SQLException {
+        int id = getNextId();
+        Movie m = new Movie(id, title, name, genre, year, duration, raiting);
+    }
+    public int getNextId() throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(nextId);
+        ResultSet rs = ps.executeQuery();
+        return rs.getInt(1);
+    };
 }
--- src/main/java/com/cinema/model/Movie.java
+++ src/main/java/com/cinema/model/Movie.java
@@ -1,24 +1,20 @@
 package com.cinema.model;
 
 public class Movie {
-    enum Status {
-        PLANNED, WATCHING, WATCHED, DROPPED
-    }
+
     private int id;
     private String title;
     private String director;
     private String genre;
     private int year;
     private int duration;
-    private Status status;
     private int raiting;
 
-    public Movie(int raiting, String genre, int year, int duration, Status status, String director, String title, int id) {
+    public Movie(int id, String title, String director, String genre, int year, int duration, int raiting) {
         this.raiting = raiting;
         this.genre = genre;
         this.year = year;
         this.duration = duration;
-        this.status = status;
         this.director = director;
         this.title = title;
         this.id = id;
@@ -32,9 +28,7 @@ public class Movie {
         this.duration = duration;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
-    }
+
 
     public void setRaiting(int raiting) {
         if (raiting < 1 || raiting > 10)
@@ -42,9 +36,11 @@ public class Movie {
         this.raiting = raiting;
     }
 
-    public void setDirector(String director) {
+    public void setDirector_firstname(String director) {
         this.director = director;
     }
+
+
 
     public void setTitle(String title) {
         this.title = title;
